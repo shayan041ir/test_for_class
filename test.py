8d669b59715ee4038d1e0e96b7a51a716771c9c7
shayan rezayi <shayanrezayi14@gmail.ccom>
2026-08-22 13:07:31 +0330

    [Add]: add while and if

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("hello world")
+# num1 = int(input("enter num 1 : "))
+# num2 = int(input("enter num 2 : "))
+
+# while num1 <= num2 :
+#     print("number : ",num1)
+#     num1 += 1
+
+name = input("enter name : ")
+num = int(input("enter num : "))
+
+if num > 20 :
+    print("pls enter num of range 0 -> 20")
+    num = int(input("enter num : "))
+
+if num >= 18 :
+    print("alef")
+elif  15 <= num < 18 :
+    print("good")
+else :
+    print("!good")
